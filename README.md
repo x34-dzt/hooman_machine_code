65b247b3a598553dd90220d3d643b31a817c0ab4
DMZTdhruv <shruvparmar@gmail.com>
2025-07-21 00:14:58 +0530

    added nix dev shell

---
+++ README.md
@@ -0,0 +1,1 @@
+# hooman_machine_code
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      name = "assembly shell";
+
+      buildInputs = [
+        pkgs.nasm
+        pkgs.gcc
+      ];
+
+      shellHook = ''
+        echo "welcome to my assembly shell"
+      '';
+    };
+  };
+}
